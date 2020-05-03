04a3ea23fb2e306a8f73bfca0bc1de834302b071
ThomsonZS <tomek.mlynek@wp.pl>
2020-05-03 22:09:26 +0200

    Add new files

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- inicjalizacja bazy danych: przykładowe dane
+
+insert into blog_post(title, content) values ('Pierwszy wpis', 'Oto pierwszy nasz wpis na blogu, krótki raczej');
+insert into blog_post(title, content) values ('Prognoza pogody', 'Jutro biomet raczej niekorzystny');
+insert into blog_post(title, content) values ('Lorem ipsum');
